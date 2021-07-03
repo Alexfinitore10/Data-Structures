326882314f7c6c5b361e79a238554d0495e257f7
Alexfinitore10 <62760619+Alexfinitore10@users.noreply.github.com>
2021-07-04 00:14:39 +0200

    C++
    Stack And Queue

---
+++ Stack And Queue/main.cpp
@@ -0,0 +1,20 @@
+
+#include "zlasdtest/test.hpp"
+#include "queue/lst/queuelst.hpp"
+#include "queue/vec/queuevec.hpp"
+#include "stack/lst/stacklst.hpp"
+#include "stack/vec/stackvec.hpp"
+#include "zmytest/test.hpp"
+
+
+/* ************************************************************************** */
+
+#include <iostream>
+
+/* ************************************************************************** */
+
+int main() {
+  std::cout << "Lasd Libraries 2020" << std::endl;
+  lasdtest(); // To call in the menu of your library test!
+  return 0;
+}
